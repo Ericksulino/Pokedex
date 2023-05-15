4ca23441efc2c80818c71d4c2d20f377db128d1a
Ericksulino <ericksulino@gmail.com>
2023-05-15 09:53:46 -0300

    finalizando...

--- scripts.js
+++ scripts.js
@@ -1,3 +1,37 @@
-fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
-.then(res => res.json())
-.then(allpokemon => console.log(allpokemon))
+pegaPokemon = (quant) =>{
+    fetch('https://pokeapi.co/api/v2/pokemon?limit='+quant)
+    .then(res => res.json())
+    .then(allpokemon => {
+        
+        var pokemons = [];
+
+        allpokemon.results.map((val)=>{
+
+            fetch(val.url)
+            .then(res => res.json())
+            .then(pokemonSingle => {
+                
+                var pokemonBoxes = document.querySelector('.pokemon-boxes');
+                pokemonBoxes.innerHTML = "";
+                pokemons.push({nome:val.name,imagem: pokemonSingle.sprites.front_default});
+
+                if(pokemons.length == quant){
+                    //final da requisição
+                    //console.log(pokemons);
+                
+                    pokemons.map((val)=>{
+                        pokemonBoxes.innerHTML+=`<div class="pokemon-box">
+                        <img src="`+(val.imagem)+`" alt="`+(val.nome)+`">
+                        <p>`+(val.nome)+`</p>
+                    </div><!--pokemon-box -->`
+                    })
+                }
+            })
+        })
+    })
+}
+var quantidade = document.getElementById("quant");
+quantidade.addEventListener("keyup",()=>{
+    pegaPokemon(quantidade.value)
+})
+pegaPokemon(30);
